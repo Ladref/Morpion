6af6b8f3c8f2156374869a4c5dfb0efb673b42af
ladref <louis.dreyfus@icloud.com>
2019-07-25 10:19:23 +0200

    test

--- app.rb
+++ app.rb
@@ -4,8 +4,4 @@ require 'json'
 require 'csv'
 Bundler.require
 
-$:.unshift File.expand_path("./../lib", __FILE__)
-require 'scrapper' #plus besoin de préciser le path exact
 
-mairies = Scrapper.new
-mairies.perform
